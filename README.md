ae25f372da0a609a332d1eee813a34c62335a531
Andrew Barclay <andrew.barclay@pixelunion.net>
2019-11-21 13:58:49 -0800

    Initial re-write

--- README.md
+++ README.md
@@ -1,3 +1,105 @@
 # Revealer
 
-Animate between states by with CSS transitions.
+Animate between states.
+
+## Basic usage
+
+[Codepen](https://codepen.io/thelornenelson/pen/oNNVrPB);
+
+```js
+import Revealer from '@pixelunion/revealer';
+
+const revealer = new Revealer();
+
+// Add a new animation using all defaults.
+const animation = revealer.add(
+  {
+    el: document.querySelector('.to-animate');
+  }
+);
+
+// Animate to state 'final';
+animation.animateTo('final');
+```
+
+```scss
+[data-revealer='initial'] {
+  display: none;
+  opacity: 0;
+  background-color: red;
+}
+
+[data-revealer-transition] {
+  display: block;
+}
+
+[data-revealer-transition="initial > final"] {
+  transition: background-color 500ms, opacity 500ms;
+}
+
+[data-revealer-transition="final > initial"] {
+  transition: background-color: 2000ms, opacity 2000ms;
+}
+
+[data-revealer='final'] {
+  background-color: blue;
+  opacity: 1;
+}
+```
+
+## Using with animations
+
+[Codepen](https://codepen.io/thelornenelson/pen/jOOROXJ)
+
+```js
+import Revealer from '@pixelunion/revealer';
+
+const revealer = new Revealer();
+
+// Add a new animation using all defaults.
+const animation = revealer.add(
+  {
+    el: document.querySelector('.to-animate'),
+    endEvent: 'animationend',
+    hold: true,
+  }
+);
+
+// Animate to state 'final';
+animation.animateTo('final');
+```
+
+```scss
+@keyframes change-color {
+  0% {
+    opacity: 0;
+    background-color: red;
+  }
+
+  50% {
+    background-color: green;
+  }
+
+  100% {
+    opacity: 1;
+    background-color: blue;
+  }
+}
+
+[data-revealer='initial'] {
+  display: none;
+}
+
+[data-revealer-transition],
+[data-revealer='final'] {
+  display: block;
+}
+
+[data-revealer-transition="initial > final"] {
+  animation: change-color 500ms normal both;
+}
+
+[data-revealer-transition="final > initial"] {
+  animation: change-color 2000ms reverse both;
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-const raf = window.requestAnimationFrame;
-
+/**
+ * Sets or removes an attribute on an element
+ * @param {HTMLElement} el Target element
+ * @param {String} name Attribute name
+ * @param {String} [value]  Attribute value. If blank, attribute will be removed.
+ */
 function attr(el, name, value) {
   if (value === undefined) {
     el.removeAttribute(name);
@@ -8,71 +12,155 @@ function attr(el, name, value) {
   }
 }
 
-// Event tracker
-function createEventTracker() {
-  const attrId = 'data-revealer-id';
-  const event = 'transitionend';
-  const map = {};
-  let uid = 0;
+/**
+ * Represents an HTML element with associate states
+ */
+class Animation {
+  /**
+   * @param {Object} options
+   * @param {HTMLElement}  options.el Target element
+   * @param {String} [options.state=initial] Initial state. This is also the default state.
+   * @param {String} [options.stateAttribute=data-revealer] Attribute name to update with state.
+   * @param {String} [options.stateChangeAttribute=data-revealer-transition] Attribute name to update with change of state.
+   * @param {String} [options.endEvent=transitionend] Event name to listen for at end of state change.
+   * @param {Boolean} [options.hold=false] If true, changeAttribute will not be removed until the next state change.
+   * @param {Function} [options.atStart] Callback to execute immediate after applying stateChangeAttribute.
+   */
+  constructor(options) {
+    this.el = options.el;
 
-  function ident(el) {
-    const id = el.getAttribute(attrId);
-    if (id !== null) return id;
+    this.state = options.state || 'initial';
+    this.initialState = this.state;
+    this.stateAttribute = options.stateAttribute || 'data-revealer';
+    this.stateChangeAttribute = options.stateChangeAttribute || 'data-revealer-transition';
+    this.endEvent = options.endEvent || 'transitionend';
+    this.hold = !!options.hold;
+    this.atStart = options.atStart || (() => { /* do nothing */ });
 
-    uid++;
-    attr(el, attrId, uid);
-    return '' + uid;
+    this.activeEventHandler = null;
   }
 
-  return function track(el, fn, d) {
-    const id = ident(el);
-
-    // Remove existing handler
-    if (map[id]) el.removeEventListener(event, map[id]);
-    if (!fn) return;
-
-    // Track new handler
-    map[id] = fn;
-    el.addEventListener(event, fn);
-  }
-}
-
-export default function revealer(options) {
-  // Options
-  options = options || {};
-  const initialState = options.initialState || 'initial';
-  const attrState = options.stateAttribute || 'data-revealer';
-  const attrTrans = options.transitionAttribute || 'data-revealer-transition';
-
-  // Events
-  const track = createEventTracker();
-
-  function is(el, state) {
-    state = state || initialState;
-    return (el.getAttribute(attrState) || initialState) === state;
+  /**
+   * Returns target element
+   *
+   * @return {HTMLElement} Target element
+   */
+  el() {
+    return this.el;
   }
 
-  function transition(el, state, cb) {
-    const from = el.getAttribute(attrState) || initialState;
-    const to = state || initialState;
-    if (from === to) return;
+  /**
+   * Returns current state
+   *
+   * @return {String} Current state
+   */
+  state() {
+    return this.state;
+  }
 
-    raf(function(){
-      attr(el, attrTrans, from + ' > ' + to);
+  /**
+   * Check if a state is active
+   * @param {String} state State to compare
+   *
+   * @return {Boolean}
+   */
+  is(state) {
+    return state === this.state;
+  }
 
-      raf(function(){
-        attr(el, attrState, to);
+  /**
+   * Sequences a change to a new state.
+   * @param {String} state Target state
+   *
+   * @return {Promise} Resolves when endEvent triggered
+   */
+  animateTo(state) {
+    const from = this.el.dataset[this.stateAttribute] || this.state;
+    const to = state || this.initialState;
 
-        track(el, function(event){
-          attr(el, attrTrans);
-          if (cb) cb(event);
+    return new Promise(resolve => {
+      if (from === to) return resolve(to, null);
+      // If hold is set, should this keep the attribute and remove the value only?
+      attr(this.el, this.stateChangeAttribute);
+
+      window.requestAnimationFrame(() => {
+        attr(this.el, this.stateChangeAttribute, from + ' > ' + to);
+        this.atStart({ el: this.el, from, to});
+
+        window.requestAnimationFrame(() => {
+          this.el.removeEventListener(this.endEvent, this.activeEventHandler);
+
+          this.activeEventHandler = e => {
+            // Ignore any events bubbling up
+            if (e.target !== this.el) return;
+            this.el.removeEventListener(this.endEvent, this.activeEventHandler);
+            if (!this.hold) {
+              attr(this.el, this.stateChangeAttribute);
+            }
+            resolve(to, e);
+          };
+
+          this.el.addEventListener(this.endEvent, this.activeEventHandler);
+
+          attr(this.el, this.stateAttribute, to);
+          this.state = to;
         });
       });
     });
   }
 
-  return {
-    is: is,
-    transition: transition,
-  };
+  /**
+   * Remove any event listeners
+   * @private
+   */
+  _unload() {
+    this.el.removeEventListener(this.endEvent, this.activeEventHandler);
+    this.activeEventHandler = null;
+  }
+}
+
+/**
+ * Manage state changes for a set of elements
+ */
+export default class Revealer {
+  constructor() {
+    this.animations = new Map();
+  }
+
+  /**
+   * Add a new element and return an animation for that element. If element already has an associated animation, return that animation.
+   * @param {Object} options
+   * @param {HTMLElement}  options.el Target element
+   * @param {String} [options.state=initial] Initial state. This is also the default state.
+   * @param {String} [options.stateAttribute=data-revealer] Attribute name to update with state.
+   * @param {String} [options.stateChangeAttribute=data-revealer-transition] Attribute name to update with change of state.
+   * @param {String} [options.endEvent=transitionend] Event name to listen for at end of state change.
+   * @param {Boolean} [options.hold=false] If true, changeAttribute will not be removed until the next state change.
+   * @param {Function} [options.atStart] Callback to execute immediate after applying stateChangeAttribute.
+   *
+   * @return {Animation}
+   */
+  add(options) {
+    if (this.animations.has(options.el)) return this.animations.get(options.el);
+
+    const animation = new Animation(options);
+    this.animations.set(options.el, animation);
+    return animation;
+  }
+
+  /**
+   * Remove a single animation
+   * @param {Animation} animation Animation to remove. Any event listeners will also be removed.
+   */
+  remove(animation) {
+    this.animations.delete(animation.el());
+    animation._unload();
+  }
+
+  /**
+   * Remove all animations, including all event listeners.
+   */
+  removeAll() {
+    this.animations.forEach(animation => animation._unload());
+  }
 }
